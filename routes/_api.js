e4c95d48119b5428e4c80f5bda0131f8c8f4508f
genki.kurita <gk_postbox1023@yahoo.co.jp>
2017-04-05 16:50:28 +0900

    Practice for Rest API.

---
+++ routes/_api.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+/* サンプルAPI③ 
+ * http://localhost:3000/samples/hello/(任意の文字列) にGETメソッドのリクエストを投げると、
+ * JSON形式で(任意の文字列)を返す。
+ */
+router.get('/certcheck', function (req, res, next) {
+
+  res.header('Content-Type', 'application/json; charset=utf-8')
+  var res_ng = {"result": "err"};
+
+  var url = req.query.url;
+  if (url == null) {
+      res.send(res_ng);
+      return;
+  }
+
+  var command = "openssl s_client -connect " + url + ":443 < /dev/null 2> /dev/null | openssl x509 -text | grep 'Not After'";
+
+  try {
+    const exec = require('child_process').exec;
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        console.log("err:" + err);
+        res.send(res_ng);
+        return;
+      }
+      var expiredDate = stdout.substr( stdout.indexOf(": ") + 2, 24);
+      var res_ok = {"result": expiredDate, "url": url};
+      res.send(res_ok);
+    });
+  } catch (e) {
+    res.send(res_ng);
+  }
+});
+
+module.exports = router;
+
